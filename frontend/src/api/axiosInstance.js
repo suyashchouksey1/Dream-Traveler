584bdd58353ccde2957f8a377a85d4751ec3464d
Suyash Chouksey <suyash18chouksey18@gmail.com>
2026-05-14 23:19:22 +0530

    Final deployment fixes

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:5000/api", // backend base URL
+    baseURL: import.meta.env.VITE_API_URL, // backend base URL
     headers: { "Content-Type": "application/json", },
 });
 
