3e7100869f2d55d640b2214ab1d1b3265c6a2055
Suyash Chouksey <suyash18chouksey18@gmail.com>
2026-05-15 01:36:42 +0530

    Responsive design added

--- frontend/src/Components/PackageCard.jsx
+++ frontend/src/Components/PackageCard.jsx
@@ -21,7 +21,7 @@ const PackageCard = ({
       <div className="package-body">
         <h3 className="package-price">{price} INR</h3>
         <div className="package-stars">★★★★★</div>
-
+ 
         <p className="package-desc">{description}</p>
 
         <div className="actions-Btn">
